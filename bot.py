4a2700987afd1063ce0da3c04fcbd1f6e7b31520
ezheng118 <ezheng118@gmail.com>
2020-09-27 02:24:53 -0400

    start of web scraping

---
+++ bot.py
@@ -0,0 +1,21 @@
+import requests
+import bs4
+import json
+#import seleniums
+
+if __name__ == "__main__":
+    param_file_loc = "./defaults.json"
+    
+    params = {}
+
+    with open(param_file_loc) as param_file:
+        json_string_data = param_file.read()
+        params = json.loads(json_string_data)
+
+    # create the correct search queries
+    searches = []
+    for key in params['queries']:
+        searches.append(params['queries'][key].replace('<location>', params['location']))
+
+    
+    
